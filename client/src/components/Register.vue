90bbaa787c888738d8f44d3d2437573182012cc5
Parth <parthkhetarpal23@gmail.com>
2020-05-25 15:59:57 -0500

    Register page complete

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -9,20 +9,37 @@ Every .vue file is seperated into 3 components.
   - methods : any type of method that you want to be able to access from your template.
 
 3. <style> </style>
-
 -->
-
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type ="email" name = "email" v-model="email" placeholder="email"/>
-    <br>
-    <input type = "password" name = "password" v-model="password" placeholder="password"/>
-    <br>
-    <div class = "error" v-html="error"/>
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-app>
+    <v-container fill-height fluid dark>
+      <v-row align="center" justify="center" dense>
+        <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+          <v-toolbar dense color="cyan">
+            <v-toolbar-title>
+              Register
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card class="elevation-12">
+            <v-card-text>
+              <br>
+              <input type ="email" name = "email" v-model="email" placeholder="email"/>
+              <br><br>
+              <input type = "password" name = "password" v-model="password" placeholder="password"/>
+              <br>
+              <div class = "error" v-html="error"/>
+              <br>
+              <v-btn class="blue" @click="register">Register</v-btn>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
--- client/src/main.js
+++ client/src/main.js
@@ -6,13 +6,13 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.config.productionTip = false
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  vuetify: new Vuetify(),
   el: '#app',
   router,
   components: { App },
